4326e394f129959b3ef3f14740c00aa2048546ae
Ultimate 365 <maidul365@gmail.com>
2025-01-10 21:42:02 +0530

    IT Reloaded

--- src/components/ITSection.jsx
+++ src/components/ITSection.jsx
@@ -440,14 +440,15 @@ export default function ITSection() {
   const month = date.getMonth() + 1;
   let thisYear, nextYear, prevYear;
   if (month < 4) {
-    thisYear = date.getFullYear() - 1;
-    nextYear = date.getFullYear();
-    prevYear = date.getFullYear() - 2;
-  } else {
     thisYear = date.getFullYear();
     nextYear = date.getFullYear() + 1;
     prevYear = date.getFullYear() - 1;
+  } else {
+    thisYear = date.getFullYear() - 1;
+    nextYear = date.getFullYear();
+    prevYear = date.getFullYear() - 2;
   }
+  const finYear = `${thisYear}-${nextYear}`;
   const [showNewModal, setShowNewModal] = useState(false);
   const [showOldModal, setShowOldModal] = useState(false);
   const [march, setMarch] = useState([]);
@@ -464,7 +465,6 @@ export default function ITSection() {
   const [february, setFebruary] = useState([]);
   const calCulateOldIT = async (data) => {
     const { id, tname, fname, school, pan, phone, disability, desig } = data;
-    const finYear = `${thisYear}-${nextYear}`;
     const marchSalary = march.filter((el) => el.id === id)[0];
     const marchArrear = marchSalary?.arrear;
     const marchBasic = marchSalary?.basic;
@@ -1069,7 +1069,6 @@ export default function ITSection() {
   };
   const calCulateNewIT = async (data) => {
     const { id, tname, school, pan, phone, disability, desig } = data;
-    const finYear = `${thisYear}-${nextYear}`;
     const marchSalary = march.filter((el) => el.id === id)[0];
     const marchArrear = marchSalary?.arrear;
     const marchBasic = marchSalary?.basic;
