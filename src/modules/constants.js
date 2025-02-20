701b32d9b79a254ef8ee21ed24999995c61b4a80
Ultimate 365 <maidul365@gmail.com>
2025-02-21 00:23:19 +0530

    Gemini AI Updated

---
+++ src/modules/constants.js
@@ -0,0 +1,14 @@
+export const MESSAGING_KEY =
+  "U2FsdGVkX1+YimvdD+8IMuYekypiXtIcjTayRYTgV/qPiOUYWWbfEpX4V3aBEKBuGHtxiclZxT5GpJQq6WEkvKO5970D5rR9TOZ7QLt020C/efRWPE2DvtUD3K90xxGWq5FViKGD5W4D+VKmzPm/ZakOqQcCY5o+6n1FjrAZDnTN0U0hMztNYeq7RSLc/pAUXZ4da9WdJfBOBCGEdNSv9DwVhi4/PbQhQGx/NI18nNU=";
+export const BOT_API_TOKEN = "U2FsdGVkX1/oYlGaobk+px0MUKz8VxJpxaIZkGaPi59AQCFH1LKj6eblxqa/5MrF+3e29sZH5vAdMPp0wgSwfqlWNCWCStjrEiqwwrCCLUo=";
+export const MEMBER_GROUP_ID = "-1002094366543";
+export const PREV6DA = 0.06;
+export const PREVDA = 0.1;
+export const DA = 0.14;
+export const NEXTDA = 0.14;
+export const HRA = 0.12;
+export const INCREAMENT = 0.06;
+export const BONUS = 5800;
+export const RESEND_API = "re_FEaRtwRb_CxAofyprbLowbWjAKgZN3ug2";
+export const ANYKEY="a5t2y8q9w0e6r4u1i7o3p5d7f2s3g1h"
+export const GEMEINI_API_KEY="U2FsdGVkX1+4dXWc7t6j9fiQXgl4+0IgXX3exIWlg2sqgKSk6A86cAxc76FRcVqEpRjwVXpYoO3IaN8/rT5E6Q=="
